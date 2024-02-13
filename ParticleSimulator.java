33b2a88d1efae6b38b1faa4a67968985d50d0812
jaiiraaaaaa <94473609+jaiiraaaaaa@users.noreply.github.com>
2024-02-13 17:41:10 +0800

    added the 3 cases
    - fix dimensions of canvas
    - particle passes tru bottom edge of canvas
    - resize should be turned off
    - dedicate a section for showing fps
    - add wall functionality
    - multithreading

--- ParticleSimulator.java
+++ ParticleSimulator.java
@@ -10,11 +10,29 @@ public class ParticleSimulator extends JFrame {
     private static final int FRAME_HEIGHT = CANVAS_HEIGHT + 100;
 
     private Canvas canvas;
-    private JTextField particleXField;
-    private JTextField particleYField;
-    private JTextField particleAngleField;
-    private JTextField particleVelocityField;
-    private JTextField particleCountField;
+
+    private JTextField numberOfParticlesField;
+    private JTextField startPointXField;
+    private JTextField startPointYField;
+    private JTextField endPointXField;
+    private JTextField endPointYField;
+    private JTextField velocityField;
+    private JTextField angleField;
+
+    private JTextField numberOfParticles2Field;
+    private JTextField particleLocationXField;
+    private JTextField particleLocationYField;
+    private JTextField startingThetaField;
+    private JTextField endingThetaField;
+    private JTextField velocity2Field;
+
+    private JTextField numberOfParticles3Field;
+    private JTextField particleLocation2XField;
+    private JTextField particleLocation2YField;
+    private JTextField startingVelocityField;
+    private JTextField endingVelocityField;
+    private JTextField thetaField;
+
     private JTextField wallX1Field;
     private JTextField wallY1Field;
     private JTextField wallX2Field;
@@ -29,36 +47,96 @@ public class ParticleSimulator extends JFrame {
         canvas = new Canvas();
         canvas.setPreferredSize(new Dimension(CANVAS_WIDTH, CANVAS_HEIGHT));
 
-        // input fields for adding particles
-        JLabel particleLabel = new JLabel("Particle Parameters:");
-        particleXField = new JTextField(5);
-        particleYField = new JTextField(5);
-        particleAngleField = new JTextField(5);
-        particleVelocityField = new JTextField(5);
-        particleCountField = new JTextField(5);
-        JButton addParticleButton = new JButton("Add Particle");
-        addParticleButton.addActionListener(new ActionListener() {
+        // Particle input fields for case 1
+        numberOfParticlesField = createTextFieldWithLabel("Number of Particles");
+        startPointXField = createTextFieldWithLabel("Start Point X");
+        startPointYField = createTextFieldWithLabel("Start Point Y");
+        endPointXField = createTextFieldWithLabel("End Point X");
+        endPointYField = createTextFieldWithLabel("End Point Y");
+        velocityField = createTextFieldWithLabel("Velocity");
+        angleField = createTextFieldWithLabel("Angle");
+
+        // Particle input fields for case 2
+        numberOfParticles2Field = createTextFieldWithLabel("Number of Particles");
+        particleLocationXField = createTextFieldWithLabel("Particle Location X");
+        particleLocationYField = createTextFieldWithLabel("Particle Location Y");
+        startingThetaField = createTextFieldWithLabel("Starting Theta");
+        endingThetaField = createTextFieldWithLabel("Ending Theta");
+        velocity2Field = createTextFieldWithLabel("Velocity");
+
+        // Particle input fields for case 3
+        numberOfParticles3Field = createTextFieldWithLabel("Number of Particles");
+        particleLocation2XField = createTextFieldWithLabel("Particle Location X");
+        particleLocation2YField = createTextFieldWithLabel("Particle Location Y");
+        startingVelocityField = createTextFieldWithLabel("Starting Velocity");
+        endingVelocityField = createTextFieldWithLabel("Ending Velocity");
+        thetaField = createTextFieldWithLabel("Theta");
+
+        // Wall input fields
+        wallX1Field = createTextFieldWithLabel("X1");
+        wallY1Field = createTextFieldWithLabel("Y1");
+        wallX2Field = createTextFieldWithLabel("X2");
+        wallY2Field = createTextFieldWithLabel("Y2");
+
+        // Particle button for case 1
+        JButton addParticleButton = createButton("Add Particles (Case 1)", new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                double x = Double.parseDouble(particleXField.getText());
-                double y = Double.parseDouble(particleYField.getText());
-                double angle = Double.parseDouble(particleAngleField.getText());
-                double velocity = Double.parseDouble(particleVelocityField.getText());
-                int count = Integer.parseInt(particleCountField.getText());
+                int count = Integer.parseInt(numberOfParticlesField.getText());
+                double startX = Double.parseDouble(startPointXField.getText());
+                double startY = Double.parseDouble(startPointYField.getText());
+                double endX = Double.parseDouble(endPointXField.getText());
+                double endY = Double.parseDouble(endPointYField.getText());
+                double velocity = Double.parseDouble(velocityField.getText());
+                double angle = Double.parseDouble(angleField.getText());
+
                 for (int i = 0; i < count; i++) {
-                    canvas.addParticle(x, y, angle, velocity);
+                    canvas.addParticle((endX - startX) * i / (count - 1) + startX,
+                            (endY - startY) * i / (count - 1) + startY,
+                            angle,
+                            velocity);
                 }
             }
         });
 
-        // input fields for adding walls
-        JLabel wallLabel = new JLabel("Wall Parameters:");
-        wallX1Field = new JTextField(5);
-        wallY1Field = new JTextField(5);
-        wallX2Field = new JTextField(5);
-        wallY2Field = new JTextField(5);
-        JButton addWallButton = new JButton("Add Wall");
-        addWallButton.addActionListener(new ActionListener() {
+        // Particle button for case 2
+        JButton addParticle2Button = createButton("Add Particles (Case 2)", new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                int count = Integer.parseInt(numberOfParticles2Field.getText());
+                double locationX = Double.parseDouble(particleLocationXField.getText());
+                double locationY = Double.parseDouble(particleLocationYField.getText());
+                double startTheta = Double.parseDouble(startingThetaField.getText());
+                double endTheta = Double.parseDouble(endingThetaField.getText());
+                double velocity = Double.parseDouble(velocity2Field.getText());
+
+                for (int i = 0; i < count; i++) {
+                    double theta = (endTheta - startTheta) * i / (count - 1) + startTheta;
+                    canvas.addParticle(locationX, locationY, theta, velocity);
+                }
+            }
+        });
+
+        // Particle button for case 3
+        JButton addParticle3Button = createButton("Add Particles (Case 3)", new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                int count = Integer.parseInt(numberOfParticles3Field.getText());
+                double locationX = Double.parseDouble(particleLocation2XField.getText());
+                double locationY = Double.parseDouble(particleLocation2YField.getText());
+                double startVelocity = Double.parseDouble(startingVelocityField.getText());
+                double endVelocity = Double.parseDouble(endingVelocityField.getText());
+                double theta = Double.parseDouble(thetaField.getText());
+
+                for (int i = 0; i < count; i++) {
+                    double velocity = (endVelocity - startVelocity) * i / (count - 1) + startVelocity;
+                    canvas.addParticle(locationX, locationY, theta, velocity);
+                }
+            }
+        });
+
+        // Wall button
+        JButton addWallButton = createButton("Add Wall", new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 int x1 = Integer.parseInt(wallX1Field.getText());
@@ -69,55 +147,86 @@ public class ParticleSimulator extends JFrame {
             }
         });
 
-        // Particle input panel
-        JPanel particleInputPanel = new JPanel();
-        particleInputPanel.setLayout(new GridLayout(2, 1));
-        JPanel particleFieldsPanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
-        particleFieldsPanel.add(new JLabel("X:"));
-        particleFieldsPanel.add(particleXField);
-        particleFieldsPanel.add(new JLabel("Y:"));
-        particleFieldsPanel.add(particleYField);
-        particleFieldsPanel.add(new JLabel("Angle:"));
-        particleFieldsPanel.add(particleAngleField);
-        particleFieldsPanel.add(new JLabel("Velocity:"));
-        particleFieldsPanel.add(particleVelocityField);
-        particleFieldsPanel.add(new JLabel("Count:"));
-        particleFieldsPanel.add(particleCountField);
-        particleInputPanel.add(particleFieldsPanel);
+        // Particle input panel for case 1
+        JPanel particleInputPanel1 = createParticleInputPanel("Case 1: ",
+                new JTextField[]{numberOfParticlesField, startPointXField, startPointYField,
+                        endPointXField, endPointYField, velocityField, angleField}, addParticleButton);
+
+        // Particle input panel for case 2
+        JPanel particleInputPanel2 = createParticleInputPanel("Case 2: ",
+                new JTextField[]{numberOfParticles2Field, particleLocationXField, particleLocationYField,
+                        startingThetaField, endingThetaField, velocity2Field}, addParticle2Button);
+
+        // Particle input panel for case 3
+        JPanel particleInputPanel3 = createParticleInputPanel("Case 3: ",
+                new JTextField[]{numberOfParticles3Field, particleLocation2XField, particleLocation2YField,
+                        startingVelocityField, endingVelocityField, thetaField}, addParticle3Button);
 
         // Wall input panel
-        JPanel wallInputPanel = new JPanel();
-        wallInputPanel.setLayout(new GridLayout(2, 1));
-        JPanel wallFieldsPanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
-        wallFieldsPanel.add(new JLabel("X1:"));
-        wallFieldsPanel.add(wallX1Field);
-        wallFieldsPanel.add(new JLabel("Y1:"));
-        wallFieldsPanel.add(wallY1Field);
-        wallFieldsPanel.add(new JLabel("X2:"));
-        wallFieldsPanel.add(wallX2Field);
-        wallFieldsPanel.add(new JLabel("Y2:"));
-        wallFieldsPanel.add(wallY2Field);
-        wallInputPanel.add(wallFieldsPanel);
+        JPanel wallInputPanel = createWallInputPanel();
 
-        // Particle button panel
-        JPanel particleButtonPanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
-        particleButtonPanel.add(addParticleButton);
+        // Separator for the divider
+        JSeparator separator = new JSeparator(JSeparator.VERTICAL);
 
-        // Wall button panel
-        JPanel wallButtonPanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
-        wallButtonPanel.add(addWallButton);
+        // Create the bottom panel with a BorderLayout
+        JPanel bottomPanel = new JPanel(new BorderLayout());
+        bottomPanel.add(separator, BorderLayout.CENTER);
+        bottomPanel.add(particleInputPanel1, BorderLayout.WEST);
+        bottomPanel.add(particleInputPanel2, BorderLayout.CENTER);
+        bottomPanel.add(particleInputPanel3, BorderLayout.EAST);
+        bottomPanel.add(wallInputPanel, BorderLayout.SOUTH);
 
         JPanel contentPane = new JPanel(new BorderLayout());
-        JPanel bottomPanel = new JPanel(new GridLayout(2, 2));
-        bottomPanel.add(particleInputPanel);
-        bottomPanel.add(particleButtonPanel);
-        bottomPanel.add(wallInputPanel);
-        bottomPanel.add(wallButtonPanel);
         contentPane.add(canvas, BorderLayout.CENTER);
         contentPane.add(bottomPanel, BorderLayout.SOUTH);
         setContentPane(contentPane);
 
         setVisible(true);
+    }
+
+    // Create a JTextField with a label
+    private JTextField createTextFieldWithLabel(String labelText) {
+        JTextField textField = new JTextField(5);
+        textField.setName(labelText); // Set the name to the label text
+        return textField;
+    }
+
+    // Create a JButton with a label
+    private JButton createButton(String labelText, ActionListener actionListener) {
+        JButton button = new JButton(labelText);
+        button.addActionListener(actionListener);
+        return button;
+    }
+
+    // Create a particle input panel with labels
+    private JPanel createParticleInputPanel(String label, JTextField[] fields, JButton button) {
+        JPanel particleInputPanel = new JPanel();
+        particleInputPanel.setLayout(new GridLayout(fields.length + 1, 2));
+        particleInputPanel.add(new JLabel(label));
+
+        for (JTextField field : fields) {
+            JPanel fieldPanel = new JPanel(new BorderLayout());
+            fieldPanel.add(new JLabel(field.getName() + ":"), BorderLayout.WEST);
+            fieldPanel.add(field, BorderLayout.CENTER);
+            particleInputPanel.add(fieldPanel);
+        }
+
+        particleInputPanel.add(button);
+        return particleInputPanel;
+    }
+
+    // Create a wall input panel
+    private JPanel createWallInputPanel() {
+        JPanel wallInputPanel = new JPanel(new GridLayout(2, 2));
+        wallInputPanel.add(new JLabel("X1:"));
+        wallInputPanel.add(wallX1Field);
+        wallInputPanel.add(new JLabel("Y1:"));
+        wallInputPanel.add(wallY1Field);
+        wallInputPanel.add(new JLabel("X2:"));
+        wallInputPanel.add(wallX2Field);
+        wallInputPanel.add(new JLabel("Y2:"));
+        wallInputPanel.add(wallY2Field);
+        return wallInputPanel;
     }
 
     public static void main(String[] args) {
